d657f37b06d2b1da3686cd2028f6ae5620e1c832
sagark161 <sagar.k161@gmail.com>
2024-12-10 10:15:03 +0530

    WIP - Extent report is not working

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,13 +3,15 @@ package runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import com.cucumber.listener.ExtentCucumberFormatter;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/HomePage.feature",
-        glue = {"stepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports/Report.html"},
-        monochrome = true
+        features =  "src/test/resources/features/HomePage.feature",
+        glue =      {"stepDefinitions"},
+        plugin =   {"pretty", "html:target/cucumber-reports/Report.html",
+                    "com.cucumber.listener.ExtentCucumberFormatter:target/MyExtentReport.html"},
+        monochrome =    true
 )
 
 public class TestRunner {
--- src/test/java/stepDefinitions/SharedSteps.java
+++ src/test/java/stepDefinitions/SharedSteps.java
@@ -1,10 +1,12 @@
 package stepDefinitions;
 
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import com.cucumber.listener.Reporter;
+
+import java.io.File;
 
 public class SharedSteps {
     WebDriver driver;
@@ -16,6 +18,8 @@ public class SharedSteps {
 
     @After
     public void tearDown(){
+        Reporter.loadXMLConfig(new File("/Users/sagar/IdeaProjects/poc-bdd/src/test/resources/Cofigurations/extent-config.xml"));
+
         driver.close();
     }
 
